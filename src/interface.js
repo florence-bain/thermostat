a73bbd9fdc3371a4467eda54c9b9c58d1ee78d72
Jessica Bulman <jessicabulman97@gmail.com>
2021-09-29 16:45:06 +0100

    added weather api

--- src/interface.js
+++ src/interface.js
@@ -1,5 +1,12 @@
 
 document.addEventListener("DOMContentLoaded", () => {
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    document.querySelector('#current-temperature').innerText = data.main.temp;
+  });
   const thermostat = new Thermostat();
   document.querySelector('#temperature').innerText = thermostat.temperature
 });
@@ -29,4 +36,18 @@ document.querySelector('#powersaving-off').addEventListener('click', () => {
   document.querySelector('#power-saving-status').innerText = 'off';
   updateTemperature();
 });
+
+const selectElement = document.querySelector('#current-city');
+selectElement.addEventListener('change', (event) => {
+  const city = event.target.value;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`
+
+  fetch(url)
+  .then((response) => {
+    return response.json()
+  })
+  .then ((data) => {
+    document.querySelector('#current-temperature').innerText = data.main.temp;
+  })
+})
 
