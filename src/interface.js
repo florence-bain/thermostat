3264eb77f5acf92c5c0efd0bcc5200a60f9d069f
Jessica Bulman <jessicabulman97@gmail.com>
2021-09-29 17:01:18 +0100

    changing weather api from button to form

--- src/interface.js
+++ src/interface.js
@@ -37,16 +37,30 @@ document.querySelector('#powersaving-off').addEventListener('click', () => {
   updateTemperature();
 });
 
-const selectElement = document.querySelector('#current-city');
-selectElement.addEventListener('change', (event) => {
-  const city = event.target.value;
+// const selectElement = document.querySelector('#current-city');
+// selectElement.addEventListener('change', (event) => {
+//   const city = event.target.value;
+//   const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`
+
+//   fetch(url)
+//   .then((response) => {
+//     return response.json()
+//   })
+//   .then ((data) => {
+//     document.querySelector('#current-temperature').innerText = data.main.temp;
+//   })
+// })
+
+document.querySelector('#select-city').addEventListener('submit', (event) => {
+  event.preventDefault();
+  const city = document.querySelector('#current-city').ariaValueMax;
   const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`
 
   fetch(url)
   .then((response) => {
     return response.json()
   })
-  .then ((data) => {
+  .then((data) => {
     document.querySelector('#current-temperature').innerText = data.main.temp;
   })
 })
